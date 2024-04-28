aea08e91ee4e5dd3a381210e13272a2b57a8ce55
Devinka Wickremeratne <devinkamw5250@gmail.com>
2024-04-28 20:22:47 +0530

    fix(conference-service): fix indentation

--- src/app/services/ConferenceService.ts
+++ src/app/services/ConferenceService.ts
@@ -15,42 +15,43 @@ export type ConferenceResponse = {
 };
 
 export const fetchAllConferences = async (
-  ): Promise<ConferenceResponse[]> => {
+): Promise<ConferenceResponse[]> => {
     try {
-      const response: AxiosResponse<ConferenceResponse[]> = await axios.get(
-        `${BASE_URL}/conferences`
-      );
-      return response.data;
+        console.log('API', BASE_URL)
+        const response: AxiosResponse<ConferenceResponse[]> = await axios.get(
+            `${BASE_URL}/conferences`
+        );
+        return response.data;
     } catch (error) {
-      console.error("Error fetching todos:", error);
-      throw error;
+        console.error("Error fetching conferences:", error);
+        throw error;
     }
-  };
+};
 
 export const fetchConferenceById = async (
     conferenceId: string
-  ): Promise<ConferenceResponse> => {
+): Promise<ConferenceResponse> => {
     try {
-      const response: AxiosResponse<ConferenceResponse> = await axios.get(
-        `${BASE_URL}/conferences/${conferenceId}`
-      );
-      return response.data;
+        const response: AxiosResponse<ConferenceResponse> = await axios.get(
+            `${BASE_URL}/conferences/${conferenceId}`
+        );
+        return response.data;
     } catch (error) {
-      console.error("Error fetching todos:", error);
-      throw error;
+        console.error("Error fetching conferences:", error);
+        throw error;
     }
-  };
+};
 
-  export const fetchReviewsByConferenceId = async (
+export const fetchReviewsByConferenceId = async (
     conferenceId: string
-  ): Promise<ConferenceResponse> => {
+): Promise<ConferenceResponse> => {
     try {
-      const response: AxiosResponse<ConferenceResponse> = await axios.get(
-        `${BASE_URL}/conferences/${conferenceId}/reviews`
-      );
-      return response.data;
+        const response: AxiosResponse<ConferenceResponse> = await axios.get(
+            `${BASE_URL}/conferences/${conferenceId}/reviews`
+        );
+        return response.data;
     } catch (error) {
-      console.error("Error fetching todos:", error);
-      throw error;
+        console.error("Error fetching conferences:", error);
+        throw error;
     }
-  }
+}
