81a384a5d450a6d1bd6d791e2ea87153ff178be4
Devinka Wickremeratne <devinkamw5250@gmail.com>
2024-04-28 18:45:32 +0530

    feat(conference-service): create conference service

---
+++ src/app/services/ConferenceService.ts
@@ -0,0 +1,56 @@
+import axios, { AxiosResponse } from 'axios';
+
+const BASE_URL = process.env.NEXT_PUBLIC_REVIEW_MANAGEMENT_BASE_URL;
+
+export type ConferenceResponse = {
+    id: number;
+    name: string;
+    description: string;
+    start_date: Date;
+    end_date: Date;
+    location: string;
+    speakers: string;
+    website: string;
+    overall_rating: number | null;
+};
+
+export const fetchAllConferences = async (
+  ): Promise<ConferenceResponse[]> => {
+    try {
+      const response: AxiosResponse<ConferenceResponse[]> = await axios.get(
+        `${BASE_URL}/conferences`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+      throw error;
+    }
+  };
+
+export const fetchConferenceById = async (
+    conferenceId: string
+  ): Promise<ConferenceResponse> => {
+    try {
+      const response: AxiosResponse<ConferenceResponse> = await axios.get(
+        `${BASE_URL}/conferences/${conferenceId}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+      throw error;
+    }
+  };
+
+  export const fetchReviewsByConferenceId = async (
+    conferenceId: string
+  ): Promise<ConferenceResponse> => {
+    try {
+      const response: AxiosResponse<ConferenceResponse> = await axios.get(
+        `${BASE_URL}/conferences/${conferenceId}/reviews`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching todos:", error);
+      throw error;
+    }
+  }
